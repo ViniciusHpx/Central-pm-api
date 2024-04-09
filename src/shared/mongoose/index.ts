a8abbb6704c4439435bac993c2cc281fcd153b54
ViniciusHpx <viniciushpx157@hotmail.com>
2024-04-09 09:55:13 -0300

    Configurado Banco de dados.

--- src/shared/mongoose/index.ts
+++ src/shared/mongoose/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const conection = mongoose
   .connect(
-    'mongodb+srv://viniciushpx150:97d5QZe1en76xsq@cluster0.1h6p0ht.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://viniciushpx150:97d5QZe1en76xsq8@cluster0.1h6p0ht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   )
   .then(() => {
     console.log('Banco de dadods conectado !');
