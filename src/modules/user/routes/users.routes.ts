5950d3a4305b0de379d7dd61ae329769f60dd0f1
ViniciusHpx <viniciushpx157@hotmail.com>
2024-04-09 09:25:51 -0300

    Adicionando autenicador na rota dos usuários.

--- src/modules/user/routes/users.routes.ts
+++ src/modules/user/routes/users.routes.ts
@@ -5,13 +5,13 @@ import isAuth from '../../../shared/http/middlewares/isAuth';
 const usersRouter = Router();
 const userController = new UserController();
 
-usersRouter.post('/', /*isAuth,*/ userController.create);
-usersRouter.get('/', /*isAuth,*/ userController.list);
-usersRouter.get('/:id', /*isAuth,*/ userController.show);
-usersRouter.delete('/:id', /*isAuth,*/ userController.delete);
-usersRouter.put('/', /*isAuth,*/ userController.update);
+usersRouter.post('/', isAuth, userController.create);
+usersRouter.get('/', isAuth, userController.list);
+usersRouter.get('/:id', isAuth, userController.show);
+usersRouter.delete('/:id', isAuth, userController.delete);
+usersRouter.put('/', isAuth, userController.update);
 usersRouter.post('/session', userController.createSession);
-usersRouter.post('/changepassword', /*isAuth,*/ userController.changePassword);
-usersRouter.post('/checksession', /*isAuth,*/ userController.checkSession);
+usersRouter.post('/changepassword', isAuth, userController.changePassword);
+usersRouter.post('/checksession', isAuth, userController.checkSession);
 
 export default usersRouter;
